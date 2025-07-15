4960f610619d2e5022003b128939c43dbb882726
Jeebendu <jeebendu2010@gmail.com>
2025-07-15 16:16:04 +0530

    updated

--- src/admin/modules/patient/components/PatientForm.tsx
+++ src/admin/modules/patient/components/PatientForm.tsx
@@ -333,7 +333,7 @@ const PatientForm = forwardRef<PatientFormRef, PatientFormProps>(({
           </FormRow>
 
           <FormRow>
-            <div className="space-y-2 w-1/2">
+            <div className="space-y-2">
               <Label>Gender <span className="text-destructive">*</span></Label>
               <RadioGroup
                 value={watchedGender}
@@ -357,7 +357,7 @@ const PatientForm = forwardRef<PatientFormRef, PatientFormProps>(({
                 <p className="text-sm text-destructive">{errors.gender.message}</p>
               )}
             </div>
-            <div className="w-1/2 flex gap-4">
+            <div className="flex gap-4">
               <div className="space-y-2 w-[70%]">
                 <Label htmlFor="dob">Date of Birth</Label>
                 <DatePicker
@@ -376,10 +376,9 @@ const PatientForm = forwardRef<PatientFormRef, PatientFormProps>(({
                   control={control}
                   name="age"
                   label="Age"
-                  type="number"
+                  type="text"
                   placeholder="Age will be calculated from DOB"
                   required
-                  disabled={true}
                   className={errors.age ? "border-red-500" : ""}
                 />
               </div>
